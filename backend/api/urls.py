2ae3f8b6dbb596e4dc24de6728d15b79d96f88da
Evgeny <ev1genial@gmail.com>
2023-07-06 12:58:43 +0300

    change urls

---
+++ backend/api/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import (IngredientViewSet, MyUserViewSet, RecipeViewSet,
+                       TagViewSet)
+
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
+router.register('users', MyUserViewSet, basename='users')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+]
