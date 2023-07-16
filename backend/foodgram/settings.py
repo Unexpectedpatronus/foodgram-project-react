51e104f4bee79d0accdf098cea732be0dcb79031
Evgeny <ev1genial@gmail.com>
2023-07-16 21:40:01 +0300

    v47

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -123,7 +123,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
@@ -136,3 +135,4 @@ DJOSER = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_URLS_REGEX = r'^/api/.*$'
