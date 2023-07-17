f1bb5de313b85b0e6db7bc12ee8c69ed7bd45e36
Evgeny <ev1genial@gmail.com>
2023-07-17 11:03:08 +0300

    v50

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,11 +10,11 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
-CSRF_TRUSTED_ORIGINS = config(
-    "CSRF_TRUSTED_ORIGINS",
-    default="http://localhost, http://127.0.0.1",
-    cast=Csv(),
-)
+# CSRF_TRUSTED_ORIGINS = config(
+#     "CSRF_TRUSTED_ORIGINS",
+#     default="http://localhost, http://127.0.0.1",
+#     cast=Csv(),
+# )
 
 INSTALLED_APPS = [
     'django.contrib.admin',
